3a9222bb790828649bbf609494861e868ca9dc1e
wdbgithub <583992377@qq.com>
2016-11-17 18:45:37 +0800

    测试22 (cherry picked from commit 3dc0c39)

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,7 +5,5 @@ public class Test {
     public static void main() {
         System.out.println("yyuyuyu");
         System.out.println("789797");
-        System.out.println("yyuyuyu5454534");
-
     }
 }
